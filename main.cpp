a53b921e5a6ed70d5ab636f82f69bb8b6363a570
Danila Smirnov <ylptred@gmail.com>
2025-06-16 17:36:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <iomanip>
 #include <cmath>
+#include <omp.h>
 
 using namespace std;
 
@@ -10,12 +11,17 @@ void printMatrix(const vector<vector<double>>& matrix, const string& name) {
     int n = matrix.size();
     cout << "\n" << name << ":\n";
     cout << fixed << setprecision(3);
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cout << setw(10) << matrix[i][j] << " ";
-        }
-        cout << "\n";
-    }
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            cout << setw(10) << matrix[i][j] << ((j == n - 1) ? "\n" : " ");
+}
+
+vector<vector<double>> generateRandomMatrix(int n) {
+    vector<vector<double>> matrix(n, vector<double>(n));
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            matrix[i][j] = ((rand() % 2001) - 1000) / 100.0;
+    return matrix;
 }
 
 bool invertMatrix(const vector<vector<double>>& input, vector<vector<double>>& inverse) {
@@ -23,7 +29,9 @@ bool invertMatrix(const vector<vector<double>>& input, vector<vector<double>>& i
     inverse.assign(n, vector<double>(n, 0.0));
 
     vector<vector<double>> aug(n, vector<double>(2 * n));
+
     for (int i = 0; i < n; ++i) {
+        #pragma GCC ivdep
         for (int j = 0; j < n; ++j)
             aug[i][j] = input[i][j];
         aug[i][n + i] = 1.0;
@@ -33,8 +41,9 @@ bool invertMatrix(const vector<vector<double>>& input, vector<vector<double>>& i
         double maxEl = fabs(aug[i][i]);
         int maxRow = i;
         for (int k = i + 1; k < n; ++k) {
-            if (fabs(aug[k][i]) > maxEl) {
-                maxEl = fabs(aug[k][i]);
+            double val = fabs(aug[k][i]);
+            if (val > maxEl) {
+                maxEl = val;
                 maxRow = k;
             }
         }
@@ -44,34 +53,31 @@ bool invertMatrix(const vector<vector<double>>& input, vector<vector<double>>& i
             return false;
         }
 
-        swap(aug[i], aug[maxRow]);
+        if (i != maxRow)
+            swap(aug[i], aug[maxRow]);
 
         double pivot = aug[i][i];
+
+        #pragma omp simd
         for (int j = 0; j < 2 * n; ++j)
             aug[i][j] /= pivot;
 
         for (int k = 0; k < n; ++k) {
-            if (k != i) {
-                double coeff = aug[k][i];
-                for (int j = 0; j < 2 * n; ++j)
-                    aug[k][j] -= coeff * aug[i][j];
-            }
+            if (k == i) continue;
+            double coeff = aug[k][i];
+
+            #pragma omp simd
+            for (int j = 0; j < 2 * n; ++j)
+                aug[k][j] -= coeff * aug[i][j];
         }
     }
 
     for (int i = 0; i < n; ++i)
+        #pragma omp simd
         for (int j = 0; j < n; ++j)
             inverse[i][j] = aug[i][j + n];
 
     return true;
-}
-
-vector<vector<double>> generateRandomMatrix(int n) {
-    vector<vector<double>> matrix(n, vector<double>(n));
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            matrix[i][j] = ((rand() % 2001) - 1000) / 100.0;
-    return matrix;
 }
 
 int main() {
@@ -85,30 +91,24 @@ int main() {
     }
 
     srand(static_cast<unsigned>(time(0)));
-
     vector<vector<double>> A = generateRandomMatrix(n);
     vector<vector<double>> A_inv;
 
     if (n <= 10)
         printMatrix(A, "The generated matrix A");
 
-    cout << "\nCalculation of the inverse matrix by the Gauss-Jordan method..." << endl;
-
-    auto start = chrono::high_resolution_clock::now();
-
+    double start = omp_get_wtime();
     bool success = invertMatrix(A, A_inv);
+    double end = omp_get_wtime();
 
-    auto end = chrono::high_resolution_clock::now();
-    auto elapsed = chrono::duration_cast<chrono::milliseconds>(end - start);
+    double elapsed_ms = (end - start) * 1000.0;
 
     if (success) {
         cout << "\nThe inverse matrix has been successfully found." << endl;
-        cout << "Lead time: " << elapsed.count() << " ms" << endl;
+        cout << "Lead time: " << fixed << setprecision(3) << elapsed_ms << " ms." << endl;
 
         if (n <= 10)
-            printMatrix(A_inv, "Inversed matrix A^(-1)");
-        else
-            cout << "The matrix is too big to display on the screen (n > 10)." << endl;
+            printMatrix(A_inv, "Inverse matrix A^(-1)");
     } else {
         cout << "The inverse matrix could not be found." << endl;
     }
